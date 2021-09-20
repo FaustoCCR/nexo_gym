b29f3b431598a6fc57a536dad9cb5d31063abf4f
Fausto <faustocamp545@gmail.com>
2021-09-20 15:51:49 -0500

    Se añadio el atributo descripcion

--- src/modelo/vo/CargoVo.java
+++ src/modelo/vo/CargoVo.java
@@ -7,10 +7,15 @@ public class CargoVo {
     //atributos
     private int id_cargo;
     private String nombre;
+    private String descripcion;
 
-    public CargoVo(int id_cargo, String nombre) {
+    public CargoVo() {
+    }
+
+    public CargoVo(int id_cargo, String nombre, String descripcion) {
         this.id_cargo = id_cargo;
         this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
     public int getId_cargo() {
@@ -28,6 +33,16 @@ public class CargoVo {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
     
     
     
