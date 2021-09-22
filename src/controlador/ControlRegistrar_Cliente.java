58eeeca5edd8788d057615902dc2ff9a2f187a2d
Fausto <faustocamp545@gmail.com>
2021-09-22 18:43:22 -0500

    Cambios para registrar cliente

--- src/controlador/ControlRegistrar_Cliente.java
+++ src/controlador/ControlRegistrar_Cliente.java
@@ -1,10 +1,19 @@
 package controlador;
 
 import java.awt.Color;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.function.Predicate;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import modelo.dao.ClienteDao;
 import modelo.dao.MembresiaDao;
@@ -14,13 +23,16 @@ import vista.VistaRegistrar_Cliente;
 
 public class ControlRegistrar_Cliente {
 
-    private ClienteDao modelo;
+    private ClienteDao modelo_cliente;
     private VistaRegistrar_Cliente vista;
     private PersonaDao modelo_persona = new PersonaDao();
     private MembresiaDao modelo_membresia = new MembresiaDao();
+    private Border origin_border = new LineBorder(Color.gray, 1);
 
-    public ControlRegistrar_Cliente(ClienteDao modelo, VistaRegistrar_Cliente vista) {
-        this.modelo = modelo;
+    boolean estado_pago = false;
+
+    public ControlRegistrar_Cliente(ClienteDao modelo_cliente, VistaRegistrar_Cliente vista) {
+        this.modelo_cliente = modelo_cliente;
         this.vista = vista;
 
         vista.setVisible(true);
@@ -29,7 +41,11 @@ public class ControlRegistrar_Cliente {
         vista.setLocationRelativeTo(null);
         vista.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        vista.getJdate_inicio().getDateEditor().setDate(new java.util.Date());
+        vista.getJdate_inicio().getDateEditor().setEnabled(false);
+
         cargarMembresias();
+        datosReferencia();
 
     }
 
@@ -43,6 +59,30 @@ public class ControlRegistrar_Cliente {
 
         });
 
+        vista.getCb_membresia().addItemListener((e) -> {
+
+            vista.getTxt_desc().setText(String.valueOf(cargarDescuento() * 100));
+            calcularTotal();
+
+        });
+        vista.getTxt_pago().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                calcularTotal();
+            }
+
+        });
+        vista.getJspinner_meses().addChangeListener(js -> calcularTotal());
+
+        vista.getBt_registrar().addActionListener(l -> registrarCliente());
+        vista.getBt_pagar().addActionListener(l -> estado_pago = true);
+
+    }
+
+    private void datosReferencia() {
+        vista.getTxt_subtotal().setText("0.0");
+        vista.getTxt_desc().setText("0.0");
+
     }
 
     private boolean buscarPersona(String cedula) {
@@ -54,19 +94,26 @@ public class ControlRegistrar_Cliente {
         busqueda = modelo_persona.mostrarDatos().stream().anyMatch(cedula_p);
 
         if (busqueda == true) {
-            System.out.println("Cedula Correcta");
             vista.getTxt_cedula().setBorder(new LineBorder(Color.decode("#6CC01B"), 2));
             modelo_persona.mostrarDatos().stream().filter(cedula_p).forEach((t) -> {
                 vista.getTxt_persona().setText(t.getNombre() + " " + t.getApellido());
 
             });
         } else {
-            System.out.println("Cedula Incorrecta");
             vista.getTxt_cedula().setBorder(new LineBorder(Color.decode("#C33529"), 2));
             vista.getTxt_persona().setText("");
 
         }
         return busqueda;
+
+    }
+
+    private int campoPersona(String cedula) {
+
+        /*Retora el id de la persona de acuerdo a su número de cédula*/
+        int id_persona = modelo_persona.mostrarDatos().stream().filter(p -> p.getDni().equals(cedula)).findAny().get().getId_persona();
+
+        return id_persona;
 
     }
 
@@ -77,6 +124,143 @@ public class ControlRegistrar_Cliente {
             vista.getCb_membresia().addItem(m.getNombre());
 
         });
+
+    }
+
+    private Date fechaInicio() {
+        Instant instant = vista.getJdate_inicio().getDate().toInstant();
+        ZoneId zid = ZoneId.of("America/Guayaquil");
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, zid);
+        Date fechainicio = Date.valueOf(zdt.toLocalDate());
+
+        return fechainicio;
+    }
+
+    private Date calcularVencimiento() {
+
+        int meses = (Integer) vista.getJspinner_meses().getValue();
+
+        LocalDate ld_vence = fechaInicio().toLocalDate().plusMonths(meses);
+        Date fecha_vence = Date.valueOf(ld_vence);
+
+        return fecha_vence;
+
+    }
+
+    private double cargarDescuento() {
+
+        int seleccion = vista.getCb_membresia().getSelectedIndex();
+        double descuento = (modelo_membresia.mostrarDatos().get(seleccion).getDescuento());
+        return descuento;
+    }
+
+    private double calcularSubtotal() {
+
+        String campo_pago = vista.getTxt_pago().getText().trim();
+        double pago;
+        int meses;
+        double subtotal;
+
+        if (campo_pago.isEmpty()) {
+
+            pago = 0;
+        } else {
+
+            pago = Double.parseDouble(campo_pago);
+
+        }
+
+        meses = (int) vista.getJspinner_meses().getValue();
+
+        if (meses > 0) {
+             subtotal = pago * meses;
+        } else {
+            subtotal = pago + meses;
+        }
+
+        return subtotal;
+
+    }
+
+    private double calcularTotal() {
+
+        double subtotal = calcularSubtotal();
+        vista.getTxt_subtotal().setText(String.valueOf(subtotal));
+        double desc = Double.parseDouble(vista.getTxt_desc().getText());
+
+        double total = subtotal - (subtotal * desc / 100);
+        vista.getTxt_total().setText(String.valueOf(total));
+        return total;
+
+    }
+
+    private void sentenciaInsert() {
+
+        int id_persona = campoPersona(vista.getTxt_cedula().getText());
+        int id_membresia = modelo_membresia.mostrarDatos().get(vista.getCb_membresia().getSelectedIndex()).getId_membresia();
+
+        modelo_cliente.setId_persona(id_persona);
+        modelo_cliente.setId_membresia(id_membresia);
+        modelo_cliente.setF_inicio(fechaInicio());
+        modelo_cliente.setF_vence(calcularVencimiento());
+        modelo_cliente.setPago(calcularTotal());
+        modelo_cliente.setEstado_pago(estado_pago);
+
+        if (modelo_cliente.insertar()) {
+
+            JOptionPane.showMessageDialog(vista, "Cliente Registrado");
+
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error al Guardar");
+        }
+
+    }
+
+    private void reiniciarCampos() {
+
+        vista.getTxt_cedula().setText("");
+        vista.getTxt_persona().setText("");
+        vista.getCb_membresia().setSelectedIndex(0);
+        vista.getJdate_inicio().setDate(new java.util.Date());
+        vista.getJspinner_meses().setValue(0);
+        vista.getTxt_pago().setText("");
+        datosReferencia();//reinicia campos contables
+        vista.getTxt_total().setText("");
+        vista.getTxt_cedula().setBorder(origin_border);
+
+    }
+
+    private boolean validarRegistro() {
+
+        String persona = vista.getTxt_cedula().getText();
+        String precio = vista.getTxt_pago().getText();
+
+        boolean validacion = true;
+
+        if (persona.isEmpty() || precio.isEmpty()) {
+
+            validacion = false;
+        }
+        return validacion;
+
+    }
+
+    private void registrarCliente() {
+
+        if (validarRegistro()) {
+
+            if (buscarPersona(vista.getTxt_cedula().getText())) {
+                sentenciaInsert();
+                reiniciarCampos();
+
+            } else {
+                JOptionPane.showMessageDialog(vista, "Cedula no identificada", "Mensaje", JOptionPane.ERROR_MESSAGE);
+            }
+
+        } else {
+
+            JOptionPane.showMessageDialog(vista, "Rellenar todos los campos ", "Mensaje", JOptionPane.ERROR_MESSAGE);
+        }
 
     }
 
--- src/vista/VistaRegistrar_Cliente.java
+++ src/vista/VistaRegistrar_Cliente.java
@@ -1,4 +1,3 @@
-
 package vista;
 
 import com.toedter.calendar.JDateChooser;
@@ -7,14 +6,11 @@ import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 
-
 public class VistaRegistrar_Cliente extends javax.swing.JFrame {
-
 
     public VistaRegistrar_Cliente() {
         initComponents();
     }
-
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -34,6 +30,13 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
         txt_pago = new javax.swing.JTextField();
         jspinner_meses = new javax.swing.JSpinner();
         jdate_inicio = new com.toedter.calendar.JDateChooser();
+        txt_subtotal = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        txt_desc = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        txt_total = new javax.swing.JTextField();
+        jLabel10 = new javax.swing.JLabel();
+        bt_pagar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,7 +44,7 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
         jLabel5.setText("Nº Meses:");
 
         jLabel6.setFont(new java.awt.Font("Trebuchet MS", 1, 16)); // NOI18N
-        jLabel6.setText("Pago:");
+        jLabel6.setText("Precio:");
 
         bt_registrar.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
         bt_registrar.setText("Registrar Cliente");
@@ -75,11 +78,41 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
         txt_pago.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
         txt_pago.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
 
+        jspinner_meses.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+
+        jdate_inicio.setForeground(new java.awt.Color(153, 153, 255));
+
+        txt_subtotal.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
+        txt_subtotal.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+        txt_subtotal.setEnabled(false);
+
+        jLabel8.setFont(new java.awt.Font("Trebuchet MS", 1, 16)); // NOI18N
+        jLabel8.setText("Subtotal:");
+
+        txt_desc.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
+        txt_desc.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+        txt_desc.setEnabled(false);
+
+        jLabel9.setFont(new java.awt.Font("Trebuchet MS", 1, 16)); // NOI18N
+        jLabel9.setText("%desc:");
+
+        txt_total.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
+        txt_total.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+        txt_total.setEnabled(false);
+
+        jLabel10.setFont(new java.awt.Font("Trebuchet MS", 1, 16)); // NOI18N
+        jLabel10.setText("Total:");
+
+        bt_pagar.setBackground(new java.awt.Color(153, 204, 255));
+        bt_pagar.setFont(new java.awt.Font("Trebuchet MS", 0, 16)); // NOI18N
+        bt_pagar.setText("Pagar");
+        bt_pagar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -90,16 +123,31 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
                         .addComponent(cb_membresia, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addComponent(jLabel7)
                     .addComponent(txt_persona, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6)
-                    .addComponent(bt_registrar, javax.swing.GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
-                    .addComponent(jLabel4)
-                    .addComponent(txt_pago, javax.swing.GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
-                    .addComponent(jspinner_meses, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jdate_inicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(30, 30, 30))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 117, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txt_desc, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(bt_pagar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jdate_inicio, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(bt_registrar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jspinner_meses, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel6)
+                                    .addComponent(txt_pago, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel9))
+                                .addGap(40, 40, 40)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel10)
+                                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(txt_subtotal, javax.swing.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
+                                    .addComponent(txt_total))))
+                        .addContainerGap(30, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -113,17 +161,19 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jdate_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(21, 21, 21)
+                        .addComponent(jdate_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jspinner_meses, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel6)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel8))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(txt_pago, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(39, 39, 39)
-                        .addComponent(bt_registrar, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txt_pago, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txt_subtotal, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(txt_cedula, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
@@ -134,7 +184,19 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(cb_membresia, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel9)
+                    .addComponent(jLabel10))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txt_desc, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_total, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(bt_pagar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(bt_registrar, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
@@ -196,6 +258,39 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
         this.txt_persona = txt_persona;
     }
 
+    public JTextField getTxt_desc() {
+        return txt_desc;
+    }
+
+    public void setTxt_desc(JTextField txt_desc) {
+        this.txt_desc = txt_desc;
+    }
+
+    public JTextField getTxt_total() {
+        return txt_total;
+    }
+
+    public void setTxt_total(JTextField txt_total) {
+        this.txt_total = txt_total;
+    }
+
+    public JTextField getTxt_subtotal() {
+        return txt_subtotal;
+    }
+
+    public void setTxt_subtotal(JTextField txt_subtotal) {
+        this.txt_subtotal = txt_subtotal;
+    }
+
+    public JButton getBt_pagar() {
+        return bt_pagar;
+    }
+
+    public void setBt_pagar(JButton bt_pagar) {
+        this.bt_pagar = bt_pagar;
+    }
+    
+    
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -231,19 +326,26 @@ public class VistaRegistrar_Cliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bt_pagar;
     private javax.swing.JButton bt_registrar;
     private javax.swing.JComboBox<String> cb_membresia;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private com.toedter.calendar.JDateChooser jdate_inicio;
     private javax.swing.JSpinner jspinner_meses;
     private javax.swing.JTextField txt_cedula;
+    private javax.swing.JTextField txt_desc;
     private javax.swing.JTextField txt_pago;
     private javax.swing.JTextField txt_persona;
+    private javax.swing.JTextField txt_subtotal;
+    private javax.swing.JTextField txt_total;
     // End of variables declaration//GEN-END:variables
 }
