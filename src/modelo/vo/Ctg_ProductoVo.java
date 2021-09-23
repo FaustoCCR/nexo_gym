9bf97a443281f9bbfe9f08b62b5f5fc0bdaea094
Fausto <faustocamp545@gmail.com>
2021-09-22 19:09:33 -0500

    Se agrego atributo descripcion

--- src/modelo/vo/Ctg_ProductoVo.java
+++ src/modelo/vo/Ctg_ProductoVo.java
@@ -7,10 +7,12 @@ public class Ctg_ProductoVo {
     //atributos
     private int id_ctgp;
     private String nombre;
+    private String descripcion;
 
-    public Ctg_ProductoVo(int id_ctgp, String nombre) {
+    public Ctg_ProductoVo(int id_ctgp, String nombre, String descripcion) {
         this.id_ctgp = id_ctgp;
         this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
     public int getId_ctgp() {
@@ -28,6 +30,16 @@ public class Ctg_ProductoVo {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
     
     
     
