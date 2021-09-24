03040868df095b35bbace13bbc54089e6409766a
EduardoUyaguari2@github.com <>
2021-09-23 19:40:45 -0500

    Registro de Rutina

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,8 +1,10 @@
 package controlador;
 
 
+import modelo.dao.RutinaDao;
 import modelo.dao.UsuarioDao;
 import vista.VistaLogin;
+import vista.VistaRegistrar_Rutina;
 
 public class Main {
 
@@ -13,6 +15,11 @@ public class Main {
 
         ControlLogin login = new ControlLogin(user, vl);
         login.funcionalidad();
+        
+//        RutinaDao mode =new RutinaDao();
+//        VistaRegistrar_Rutina vis = new VistaRegistrar_Rutina();
+//        ControlRegistrar_Rutina cont =new ControlRegistrar_Rutina(mode, vis);
+//        cont.funcionalidad();
 
     }
 
