0c6f9474f094eb7b10d9204ee05ab11d6c84d378
CristhianEPC <cristhian.pallmay.est@tecazuay.edu.ec>
2021-10-05 12:56:23 -0600

    Creacion de vista para modificar, botones modificar y eliminar.

--- src/controlador/ControlGestion_Persona.java
+++ src/controlador/ControlGestion_Persona.java
@@ -6,13 +6,17 @@
 package controlador;
 
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import modelo.dao.PersonaDao;
+import vista.VistaActualizar_Persona;
 import vista.VistaGestion_Persona;
 
 /**
@@ -24,6 +28,7 @@ public class ControlGestion_Persona {
     private VistaGestion_Persona vista_persona;
     private DefaultTableModel tb_model;
     private Object[] columnas = {"ID", "Nombre", "Edad", "Teléfono", "Dirección", "Correo"};
+     public static int id_persona;
 
     public ControlGestion_Persona(PersonaDao modelo_persona, VistaGestion_Persona vista_persona) {
         this.modelo_persona = modelo_persona;
@@ -36,10 +41,22 @@ public class ControlGestion_Persona {
         vista_persona.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         disenioTabla();
 
-        mostrarDatosTabla();
+        mostrarDatosTabla("");
     }
     
     public void funcionalidad() {
+           vista_persona.getTxt_buscar().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+                mostrarDatosTabla(vista_persona.getTxt_buscar().getText());
+
+            }
+
+        });
+
+        vista_persona.getBt_verificar().addActionListener(l -> ventanaActualizar());
+        vista_persona.getBt_eliminar().addActionListener(l->sentenciaDelete());
 
     }
     
@@ -69,17 +86,37 @@ public class ControlGestion_Persona {
 
     }
     
-    private void mostrarDatosTabla() {
+    private void mostrarDatosTabla(String aguja) {
 
         tb_model.setRowCount(0);
-        modelo_persona.mostrarDatosJoin().stream().forEach((p) -> {
+        modelo_persona.mostrarDatosJoin(aguja).stream().forEach((p) -> {
 
             Object[] contenido
-                    = {p.getId_persona(), p.getNombre() + " " + p.getApellido(), Calcular_Edad(p.getBirthdate()), p.getTelefono(), p.getDireccion(), p.getCorreo()};
+                    = {p.getId_persona(), p.getNombre(), Calcular_Edad(p.getBirthdate()), p.getTelefono(), p.getDireccion(), p.getCorreo()};
 
             tb_model.addRow(contenido);
 
         });
+    }
+    
+     private void ventanaActualizar() {
+
+        int fila = vista_persona.getJtable_persona().getSelectedRow();
+        final int columna = 0;
+
+        if (fila != -1) {
+
+            id_persona = (int) vista_persona.getJtable_persona().getValueAt(fila, columna);
+            System.out.println("funciona");
+            vista_persona.dispose();
+            VistaActualizar_Persona vista = new VistaActualizar_Persona();
+            ControlActualizar_Persona control = new ControlActualizar_Persona(modelo_persona, vista);
+            control.funcionalidad();
+
+        } else {
+            JOptionPane.showMessageDialog(vista_persona, "Seleccione el registro a verificar");
+        }
+
     }
     
     public String Calcular_Edad(Date fecha){
@@ -91,6 +128,29 @@ public class ControlGestion_Persona {
         Period periodo = Period.between(fechanace, fechaactual);
         String edad = (" " + periodo.getYears()+" Años");
         return edad;
+    }
+    
+    private void sentenciaDelete() {
+
+        int fila = vista_persona.getJtable_persona().getSelectedRow();
+        final int columna = 0;
+        if (fila != -1) {
+
+            int id_persona = (int) vista_persona.getJtable_persona().getValueAt(fila, columna);
+            String nombre = modelo_persona.mostrarDatos().stream().filter(u -> u.getId_persona()== id_persona).findAny().get().getNombre();
+
+            int resp = JOptionPane.showConfirmDialog(vista_persona, "Seguro desea eliminar al usuario : " + nombre, "Confirmación", JOptionPane.YES_NO_OPTION);
+            if (resp == 0) {
+
+                modelo_persona.eliminar(id_persona);
+                JOptionPane.showMessageDialog(vista_persona, "Registro Eliminado");
+                mostrarDatosTabla("");
+
+            }
+
+        } else {
+            JOptionPane.showMessageDialog(vista_persona, "Seleccione el registro a eliminar");
+        }
     }
     
 }
--- src/modelo/dao/PersonaDao.java
+++ src/modelo/dao/PersonaDao.java
@@ -55,11 +55,14 @@ public class PersonaDao extends PersonaVo {
         }
     }
     
-    public List<PersonaVo> mostrarDatosJoin() {
+    public List<PersonaVo> mostrarDatosJoin(String aguja) {
 
         List<PersonaVo> lista_clientes = new ArrayList<>();
-        String sql = "select id_persona, nombre, apellido, birthdate, correo, direccion, telefono"
-                + " from persona;";
+        String sql = "select p.id_persona, p.nombre||' '||p.apellido \"Persona\", p.birthdate, p.correo, p.direccion, p.telefono"
+                + " from persona p"
+                + " where upper(p.nombre||' '||p.apellido)  like upper('%" + aguja + "%') or"
+                + " upper(p.dni) like upper('%" + aguja + "%')"
+                + " order by 1;";
         ResultSet rs = conecta.consulta(sql);
 
         try {
@@ -70,11 +73,11 @@ public class PersonaDao extends PersonaVo {
 
                 pl.setId_persona(rs.getInt(1));
                 pl.setNombre(rs.getString(2));
-                pl.setApellido(rs.getString(3));
-                pl.setBirthdate(rs.getDate(4));
-                pl.setCorreo(rs.getString(5));
-                pl.setDireccion(rs.getString(6));
-                pl.setTelefono(rs.getString(7));
+               // pl.setApellido(rs.getString(3));
+                pl.setBirthdate(rs.getDate(3));
+                pl.setCorreo(rs.getString(4));
+                pl.setDireccion(rs.getString(5));
+                pl.setTelefono(rs.getString(6));
 
                 lista_clientes.add(pl);
 
@@ -87,13 +90,86 @@ public class PersonaDao extends PersonaVo {
             return null;
         }
     }
+    
+     public List<PersonaVo> mostrarDatosJoin(int id_persona) {
+
+        List<PersonaVo> lista_persona = new ArrayList<>();
+        String sql = "select * "
+                + " from persona "
+                + " where id_persona = '" + id_persona + "'\n"
+                + " order by 1;";
+        ResultSet rs = conecta.consulta(sql);
+
+        try {
+
+            while (rs.next()) {
+
+                PersonaVo pl = new PersonaDao();
+
+                pl.setId_persona(rs.getInt(1));
+                pl.setDni(rs.getString(2));
+                pl.setNombre(rs.getString(3));
+                pl.setApellido(rs.getString(4));
+                pl.setBirthdate(rs.getDate(5));
+                pl.setCorreo(rs.getString(6));
+                pl.setGenero(rs.getString(7).charAt(0));
+                pl.setDireccion(rs.getString(8));
+                pl.setTelefono(rs.getString(9));
+
+                lista_persona.add(pl);
+
+            }
+
+            rs.close();//cerramos conexion base.
+            return lista_persona;
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonaDao.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+     
+     public int contadorPer(String cedula){
+        int con=0;
+    String sql="SELECT COUNT(dni) "
+            + " FROM persona"
+            + " where dni = '"+cedula+"'";
+          ResultSet rs = conecta.consulta(sql);
+        try {
+            while (rs.next()) {                
+                con=rs.getInt(1);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return con;
+    }
 
     public boolean grabar() {
         String sql;
-        sql = "INSERT INTO persona(dni,nombre,apellido,birthdate,correo,genero,direccion,telefono) ";
-        sql += " VALUES ('" + getDni() + "','" + getNombre() + "','" + getApellido() + "','" + getBirthdate() + "','" + getCorreo() + "','" + getGenero() + "','" + getDireccion() + "','" + getTelefono() + "')";
+        sql = "INSERT INTO persona(id_persona,dni,nombre,apellido,birthdate,correo,genero,direccion,telefono) ";
+        sql += " VALUES (" + getId_persona() + ",'" + getDni() + "','" + getNombre() + "','" + getApellido() + "','" + getBirthdate() + "','" + getCorreo() + "','" + getGenero() + "','" + getDireccion() + "','" + getTelefono() + "')";
         return conecta.accion(sql);
 
+    }
+    
+    public boolean modificar1(int identificador) {
+
+        String sql;
+        sql = "UPDATE persona set \"dni\"='" + getDni() + "',\"nombre\"='" + getNombre()
+                + "',\"apellido\"='" + getApellido() + "',\"birthdate\"='" + getBirthdate() 
+                + "',\"correo\"='" + getCorreo() + "',\"genero\"='" + getGenero()
+                + "',\"direccion\"='" + getDireccion() + "',\"telefono\"='" + getTelefono() + "'"
+                + "WHERE \"id_persona\"='" + identificador + "'";
+
+        return conecta.accion(sql);
+    }
+    
+    public boolean eliminar(int identificador) {
+
+        String sql = "delete from persona where \"id_persona\"='" + identificador + "'";
+
+        return conecta.accion(sql);
     }
 
 }
