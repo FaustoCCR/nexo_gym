61ac3db0acfe684c5b7e4b1abc482246bc53c7b0
CristhianEPC <cristhian.pallmay.est@tecazuay.edu.ec>
2021-10-07 23:05:01 -0600

    Creacion de vista categorias de productos para modificar, implementacion de botones para modificar y eliminar.

---
+++ src/controlador/ControlActualizar_CtgProducto.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.function.Predicate;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import modelo.dao.Ctg_ProductoDao;
+import modelo.vo.Ctg_ProductoVo;
+import vista.VistaActualizar_CtgProducto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ControlActualizar_CtgProducto {
+    private Ctg_ProductoDao modelo_ctgp;
+    private VistaActualizar_CtgProducto vista_ctgp;
+    private int id_ctgp;
+    private Border origin_border = new LineBorder(Color.gray, 1);
+    
+    public ControlActualizar_CtgProducto(Ctg_ProductoDao modelo_ctgp, VistaActualizar_CtgProducto vista_ctgp) {
+        this.modelo_ctgp = modelo_ctgp;
+        this.vista_ctgp = vista_ctgp;
+
+        id_ctgp = ControlGestion_CtgProducto.id_ctgp;
+
+        vista_ctgp.setVisible(true);
+        vista_ctgp.setTitle("Actualizar Rutina - Nexo Gym");
+        vista_ctgp.setResizable(false);
+        vista_ctgp.setLocationRelativeTo(null);
+        vista_ctgp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        cargarDatosCtgProducto();
+
+    }
+    
+    public void funcionalidad() {
+        vista_ctgp.getTxt_nombre().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                verificarCategoria(vista_ctgp.getTxt_nombre().getText());
+            }
+
+        });
+        vista_ctgp.getBt_actualizar().addActionListener(l -> actualizarCtgProducto());
+    }
+    
+    private void cargarDatosCtgProducto() {
+
+        modelo_ctgp.mostrarDatosJoin(id_ctgp).stream().forEach((us) -> {
+
+            vista_ctgp.getTxt_nombre().setText(us.getNombre());
+            vista_ctgp.getTxt_descripcion().setText(us.getDescripcion());
+
+        });
+
+    }
+    
+    private boolean validarRegistro() {
+
+        String nombre = vista_ctgp.getTxt_nombre().getText();
+        String descripcion = vista_ctgp.getTxt_descripcion().getText();
+
+        boolean validacion = true;
+
+        if (nombre.isEmpty() || descripcion.isEmpty()) {
+
+            validacion = false;
+        }
+        return validacion;
+
+    }
+    
+    private boolean verificarCtgProducto(String nombre) {
+
+        boolean busqueda = Validar_Categoria_Nueva();
+        if (!nombre.isEmpty()) {
+            if (busqueda) {
+                vista_ctgp.getTxt_nombre().setBorder(new LineBorder(Color.decode("#6CC01B"), 2));
+            } else {
+                vista_ctgp.getTxt_nombre().setBorder(new LineBorder(Color.decode("#C33529"), 2));
+            }
+            return busqueda;
+
+        } else {
+            vista_ctgp.getTxt_nombre().setBorder(new LineBorder(Color.decode("#C33529"), 2));
+            return false;
+        }
+    }
+    
+    private void sentenciaUpdate() {
+
+        String nombre = vista_ctgp.getTxt_nombre().getText().trim();
+        String descripcion = vista_ctgp.getTxt_descripcion().getText().trim();
+
+        modelo_ctgp.setNombre(nombre);
+        modelo_ctgp.setDescripcion(descripcion);
+
+        if (modelo_ctgp.modificar1(id_ctgp)) {
+
+            JOptionPane.showMessageDialog(vista_ctgp, "Rutina Actualizada");
+
+        } else {
+            JOptionPane.showMessageDialog(vista_ctgp, "Error al Actualizar");
+        }
+
+    }
+    
+    private void restaurarBordes() {
+
+        vista_ctgp.getTxt_nombre().setBorder(origin_border);
+    }
+
+    private void actualizarCtgProducto() {
+
+        if (validarRegistro()) {
+
+            if (verificarCtgProducto(vista_ctgp.getTxt_nombre().getText())) {
+
+                sentenciaUpdate();
+                restaurarBordes();
+
+            } else {
+                JOptionPane.showMessageDialog(vista_ctgp, " Categoría no disponible", "Advertencia", JOptionPane.ERROR_MESSAGE);
+                vista_ctgp.getTxt_nombre().grabFocus();
+            }
+        }else {
+            JOptionPane.showMessageDialog(vista_ctgp, "Rellenar todos los campos ", "Mensaje", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+    
+    
+    private int verificarCategoria(String nombre) {
+
+        int cont = modelo_ctgp.contadorCtg(nombre);
+//        System.out.println(cont);
+        return cont;
+
+    }
+    
+    private int campoId_Categoria(String nombre) {
+
+        int id_ctg;
+        id_ctg = modelo_ctgp.mostrarDatosJoin(nombre).stream().filter(p -> p.getNombre().equalsIgnoreCase(nombre)).findAny().get().getId_ctgp();
+        return id_ctg;
+
+    }
+    
+    private boolean Validar_Categoria_Nueva() {
+
+        String nombre2 = vista_ctgp.getTxt_nombre().getText();
+
+        if (verificarCategoria(nombre2) >= 1) {
+            int id1 = campoId_Categoria(nombre2);
+            if (id1 == id_ctgp) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+
+    }
+
+    
+}
--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,17 +1,24 @@
 package controlador;
 
+import modelo.dao.Ctg_ProductoDao;
 import modelo.dao.UsuarioDao;
+import vista.VistaGestion_CtgProducto;
 import vista.VistaLogin;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        UsuarioDao user = new UsuarioDao();
-        VistaLogin vl = new VistaLogin();
-
-        ControlLogin login = new ControlLogin(user, vl);
-        login.funcionalidad();
+//        UsuarioDao user = new UsuarioDao();
+//        VistaLogin vl = new VistaLogin();
+//
+//        ControlLogin login = new ControlLogin(user, vl);
+//        login.funcionalidad();
+        
+        Ctg_ProductoDao mode = new Ctg_ProductoDao();
+        VistaGestion_CtgProducto vista = new VistaGestion_CtgProducto();
+        ControlGestion_CtgProducto control = new ControlGestion_CtgProducto(mode, vista);
+        control.funcionalidad();
         
     }
 
